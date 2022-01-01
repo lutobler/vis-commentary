1cdc2e56e5f26aed14c0fc44d0711357b18f9a7f
Romain Hervier <romain@sansfontieres.com>
2022-01-01 01:39:52 +0100

    Add support for Zig

--- init.lua
+++ init.lua
@@ -21,7 +21,7 @@ local comment_string = {
     rexx='--', rhtml='<!--|-->', rstats='#', ruby='#', rust='//', sass='//',
     scala='//', scheme=';', smalltalk='"|"', sml='(*)', snobol4='#', sql='#',
     tcl='#', tex='%', text='', toml='#', vala='//', vb='\'', vbscript='\'',
-    verilog='//', vhdl='--', wsf='<!--|-->', xml='<!--|-->', yaml='#'
+    verilog='//', vhdl='--', wsf='<!--|-->', xml='<!--|-->', yaml='#', zig='//'
 }
 
 -- escape all magic characters with a '%'
